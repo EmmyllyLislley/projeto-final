eb756769651956ef8670b97ccd2ca106d4fba3a7
Seu Nome <ellr1@aluno.ifal.edu.br>
2026-05-15 13:15:27 -0300

    Criação da classe modelo de usuario

---
+++ models/usuarioModel.js
@@ -0,0 +1,46 @@
+class Usuario {
+    #id
+    #nome
+    #email
+    #senha
+
+    constructor(id, nome, email, senha) {
+        this.#id = id;
+        this.#nome = nome;
+        this.#email = email;
+        this.#senha = senha;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get nome() {
+        return this.#nome;
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    set nome(novo_nome) {
+        if(!novo_nome) {
+            throw new Error("Insira um nome")
+        }
+        this.#nome = novo_nome;
+    }
+
+    set email(novo_email) {
+        if (!novo_email.includes("@")) {
+            throw new Error("Email inválido!");
+        }
+        this.#email = novo_email;
+    }
+
+    alterarSenha(senhaAtual, novaSenha) {
+        if(senhaAtual !== this.#senha) {
+            throw new Error("Senha incorreta");
+        }
+        this.#senha = novaSenha;
+    }
+}
