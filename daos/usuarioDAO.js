6f2d4745ee018675897fdf10a1657b421c0924c6
Seu Nome <ellr1@aluno.ifal.edu.br>
2026-06-06 13:17:53 -0300

    atualizando as funções

--- daos/usuarioDAO.js
+++ daos/usuarioDAO.js
@@ -10,13 +10,13 @@ class UsuarioDAO {
         const sql = `CREATE TABLE IF NOT EXISTS usuarios (
                 id INT PRIMARY KEY AUTO_INCREMENT,
                 nome VARCHAR(100),
-                email VARCHAR(100),
-                senha VARCHAR(8)
+                email VARCHAR(100) UNIQUE,
+                senha VARCHAR(100)
         )`
 
         this.connection.query(sql, (err) => {
             if(err) {
-                console.log("Erro ao criar a tabela usuários: " + err);
+                console.error("Erro ao criar a tabela usuários: " + err);
                 return;
             }
 
@@ -29,7 +29,7 @@ class UsuarioDAO {
 
         this.connection.query(sql, (err) => {
             if(err) {
-                console.log("Erro ao remover tabela usuários: " + err);
+                console.error("Erro ao remover tabela usuários: " + err);
                 return;
             }
 
@@ -40,12 +40,14 @@ class UsuarioDAO {
     adicionarUsuario(usuario) {
         const sql = `INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)`;
 
-        this.connection.query(sql, [usuario.nome, usuario.email, usuario.senha], (err) => {
-            if (err) {
-                console.log("Erro ao adicionar usuário: ", err);
-                return;
-            }
-            console.log("Usuário cadastrado!");
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, [usuario.nome, usuario.email, usuario.senha], (err, result) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(result);
+            });
         });
     }
 
@@ -54,52 +56,57 @@ class UsuarioDAO {
                     SET nome = ?, email = ?
                     WHERE id = ?`;
 
-        this.connection.query(sql, [novoUsuario.nome, novoUsuario.email, id], (err) => {
-            if(err) {
-                console.log("Erro ao atualizar usuário: " + err);
-                return;
-            }
-            console.log("Usuário atualizado com sucesso!")
-        })
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, [novoUsuario.nome, novoUsuario.email, id], (err, result) => {
+                if(err) {
+                    reject(err);
+                    return;
+                }
+                resolve(result)
+            });
+        });
     }
 
     removerUsuario(id) {
         const sql = `DELETE FROM usuarios WHERE id = ?`;
 
-        this.connection.query(sql, [id], (err) => {
-            if(err) {
-                console.log("Erro ao excluír usuário: " + err);
-                return;
-            }
-            console.log("Usuário excluído!")
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, [id], (err, result) => {
+                if(err) {
+                    reject(err);
+                    return;
+                }
+                resolve(result)
+            });
         });
     }
 
     listarUsuarios() {
         const sql = `SELECT * FROM usuarios`;
 
-        this.connection.query(sql, (err, result) => {
-            if(err) {
-                console.log("Erro ao listar usuários: " + err);
-                return;
-            }
-            console.log("Lista completa de usuários!");
-            console.log(result);
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, (err, result) => {
+                if(err) {
+                    reject(err);
+                    return;
+                }
+                resolve(result);
+            });
         });
     }
 
     buscarUsuarioPorEmail(email) {
-        const sql = `SELECT * FROM usuarios
-                    WHERE email = ?`;
+        const sql = `SELECT * FROM usuarios WHERE email = ?`;
 
-        this.connection.query(sql, [email], (err, result) => {
-                if(err) {
-                    console.log(err);
-                    return;
-                }
-                console.log(result);
-            }
-        );
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, [email], (err, result) => {
+                    if(err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(result[0]);
+            });
+        });
     }
 }
 
